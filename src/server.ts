71d900c7554295606a7ebad99ebe0469b2631de6
Nortoy <tolipovhusniddin2005@gmail.com>
2026-09-24 18:10:03 +0900

    feat: connect mongoDB via mongoose

--- src/server.ts
+++ src/server.ts
@@ -16,3 +16,15 @@ dotenv.config();
 
 
 // CLUSTER => DATABASE => COLLECTION => DOCUMENT
+
+
+
+ import mongoose from "mongoose";
+
+ mongoose
+ .connect(process.env.MONGO_URL as string, {})
+ .then((data) => {
+    console.log("MongoDb connection succeed");
+    const PORT = process.env.PORT ?? 5005;
+ })
+ .catch((err) => console.log("ERROR on connection MongoDB", err));
