6ffda97c8e8a4e74acd73b7af59c8537e08f5735
Nortoy <tolipovhusniddin2005@gmail.com>
2026-09-24 18:41:48 +0900

    feat: express integrations

--- src/server.ts
+++ src/server.ts
@@ -4,27 +4,22 @@
 
 // Design pattern: Midddleware, Decotar
 
-
-
-import dotenv from "dotenv";
-dotenv.config();
-
-// console.log("PORT:", process.env.PORT);
-
-// console.log("MONGO_URL:", process.env.MONGO_URL);
-
-
-
 // CLUSTER => DATABASE => COLLECTION => DOCUMENT
 
 
 
- import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+import mongoose from "mongoose";
+import app from "./app";
 
  mongoose
  .connect(process.env.MONGO_URL as string, {})
  .then((data) => {
-    console.log("MongoDb connection succeed");
+    console.log("MongoDB connection succeed");
     const PORT = process.env.PORT ?? 5005;
+    app.listen(PORT, function () {
+        console.log(`The server is running succssfully on port: ${PORT}`);
+    });
  })
  .catch((err) => console.log("ERROR on connection MongoDB", err));
